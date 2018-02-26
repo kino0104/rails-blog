75773d5ea8114f73b5d422728d5e8aae268a1e38
kino0104a <kyousuke.kinoshita@gmail.com>
2018-02-27 07:35:10 +0900

    コメント機能追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+    has_many :comments, dependent: :destroy
     validates :title, presence: true, length: { minimum: 3, message: 'タイトルは3文字以上で入力してください'}
     validates :body, presence: true
 end
